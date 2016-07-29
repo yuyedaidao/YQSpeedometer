c2332c8c5a10a1edc5a0488bc0eb670af3ed8919
padding <wyqpadding@gmail.com>
2016-07-29 10:02:45 +0800

    Initial Commit

---
+++ YQSpeedometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQSpeedometer
+//
+//  Created by Wang on 16/7/29.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQSpeedometer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YQSpeedometer
+//
+//  Created by Wang on 16/7/29.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
